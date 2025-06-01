1e85e03db986bd35c8f5741022e1f1d99c0a1b11
dev-debabrata <debabratadas711@gmail.com>
2025-06-01 23:51:35 +0530

    01-06-2025

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            <nav className=" custom flex flex-row justify-around items-center py-4 md:py-8 uppercase font-semibold relative text-xs md:text-lg">
+            <nav className="custom flex flex-row justify-around items-center py-4 md:py-8 uppercase font-semibold relative text-xs md:text-lg">
                 <a href='#'>About</a>
                 <a href='#'>Explore</a>
                 <a href='#'></a>
